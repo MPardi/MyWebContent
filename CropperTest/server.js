f5beee25d8a6763dfc57a7c382a7abc0f0cfb357
mpardi <mpardi@asu.edu>
2017-11-02 11:29:21 -0700

    Added CropperJS test

--- CropperTest/server.js
+++ CropperTest/server.js
@@ -5,24 +5,9 @@ var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8080;
-var Cropper = require('cropperjs');
 
 app.get("/", function (req, res) {
       res.redirect("/index.html");
-});
-
-var image = document.getElementById('image');
-var cropper = new Cropper(image, {
-  aspectRatio: 16 / 9,
-  crop: function(e) {
-    console.log(e.detail.x);
-    console.log(e.detail.y);
-    console.log(e.detail.width);
-    console.log(e.detail.height);
-    console.log(e.detail.rotate);
-    console.log(e.detail.scaleX);
-    console.log(e.detail.scaleY);
-  }
 });
 
 app.use(methodOverride());
