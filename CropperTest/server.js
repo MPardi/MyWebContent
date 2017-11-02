07180afe5144411f4e71fc8d5eeff003756bb301
mpardi <mpardi@asu.edu>
2017-11-02 11:25:39 -0700

    Added CropperJS test

--- CropperTest/server.js
+++ CropperTest/server.js
@@ -11,6 +11,20 @@ app.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
+var image = document.getElementById('image');
+var cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  crop: function(e) {
+    console.log(e.detail.x);
+    console.log(e.detail.y);
+    console.log(e.detail.width);
+    console.log(e.detail.height);
+    console.log(e.detail.rotate);
+    console.log(e.detail.scaleX);
+    console.log(e.detail.scaleY);
+  }
+});
+
 app.use(methodOverride());
 app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
